e7b045d878eac87b3194322fa68d621fc8ee1e24
unknown <syyadkhan@yahoo.com>
2018-06-10 17:06:01 -0400

    ejs and requires

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var express = require("express")
-var mongoose = require("mongoose")
-var bodyParser = require("body-parser")
-var app = express()
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var app = express();
+
+mongoose.connect("mongodb://localhost/blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+title
+image
+body
+created
+
+
 
