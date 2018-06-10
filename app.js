fc252cdf84e9d05be29cd11fd06bc105b38f9bf9
unknown <syyadkhan@yahoo.com>
2018-06-10 18:07:45 -0400

    seed blog post

--- app.js
+++ app.js
@@ -6,11 +6,39 @@ var app = express();
 mongoose.connect("mongodb://localhost/blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
-title
-image
-body
-created
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default:Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// 7 restful routes
+app.get("/", function(req,res){
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("error");
+        }else{
+            res.render("index", {blogs: blogs});
+        }
+    });    
+});
 
 
+
+
+
+
+var PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+    console.log('server on port 3000');
+});
 
